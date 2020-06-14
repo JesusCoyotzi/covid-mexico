3515f23333066bf98b1e436139a4a1c3c57da03c
JesusCoyotzi <jesus_coyotzi@hotmail.com>
2020-06-13 19:32:15 -0500

    added bar plots

--- covid_salubridad.py
+++ covid_salubridad.py
@@ -1,9 +1,32 @@
 import pandas as pd 
 import matplotlib.pyplot as plt 
+import matplotlib.dates as mdates
+from matplotlib.dates import DateFormatter
 
 import datetime
 import argparse
 import sys
+
+def graph_df(df,cumulative=False, title="Cases"):
+  """Graph cases per dar, df has to have proper format"""
+  fig, ax = plt.subplots(figsize=(15,7))
+
+  if cumulative:      
+     df = coalesced_df.cumsum()
+     df.plot(grid=True,ax=ax)
+  else:
+     ax.bar(df.index,df['Sospechosos'],label='Sospechosos',bottom=df['Positivos'])
+     ax.bar(df.index,df['Positivos'],label='Positivos')
+  
+  date_form = DateFormatter('%d-%m-%Y')
+  ax.set_title(title)
+  ax.xaxis_date()
+  ax.xaxis.set_major_locator(mdates.WeekdayLocator())
+  ax.xaxis.set_major_formatter(date_form)
+  ax.xaxis.set_tick_params(rotation=45)
+  ax.legend()
+  
+  
 if __name__=='__main__':
 
   parser = argparse.ArgumentParser(description="Datos oficiales Covid-19")
@@ -26,7 +49,8 @@ if __name__=='__main__':
   covid_df['FECHA_SINTOMAS'] = pd.to_datetime(covid_df['FECHA_SINTOMAS'])
 
   if parsed.estado:
-    estado_sel = estado_df[estado_df['ENTIDAD_FEDERATIVA'] == parsed.estado]
+    estado = parsed.estado.upper()
+    estado_sel = estado_df[estado_df['ENTIDAD_FEDERATIVA'] == estado]
     if estado_sel.empty:
       print("El estado {} no existe, favor de verificar".format(parsed.estado))
       sys.exit(1)
@@ -48,35 +72,47 @@ if __name__=='__main__':
   negative_df = covid_df[covid_df['RESULTADO'] == 2]
   print("Casos negativos     || {}".format(negative_df['ID_REGISTRO'].count()))
   pending_df = covid_df[covid_df['RESULTADO'] == 3]
-  print("Casos pendientes    || {}".format(pending_df['ID_REGISTRO'].count()))
- 
+  print("Casos sospechosos   || {}".format(pending_df['ID_REGISTRO'].count()))
+  #active_df = covid_df[covid_df['RESULTADO'] != 3]
+
   #Did not used all of the categories but left code for referenced
   timeseries_full = covid_df.groupby('FECHA_SINTOMAS') 
   timeseries_positive  = positive_df.groupby('FECHA_SINTOMAS')
+  timeseries_pending  = pending_df.groupby('FECHA_SINTOMAS')
   #timeseries_negative  = negative_df.groupby('FECHA_SINTOMAS')
-  #timeseries_pending  = pending_df.groupby('FECHA_SINTOMAS')
 
+  print("------------------------------")
   death_df = covid_df.loc[covid_df['FECHA_DEF'] != "9999-99-99"].copy()
   death_df['FECHA_DEF'] = pd.to_datetime(death_df['FECHA_DEF'],format="%Y-%m-%d")
-  death_df = death_df.groupby('FECHA_DEF')
+  if parsed.desplazamiento:
+    death_df = death_df[death_df['FECHA_DEF'] < last_date]
 
-  coalesced_df = pd.DataFrame(columns=["Positivos", "Defunciones"])
+  print("Defunciones registradas ||  {}".format(death_df['ID_REGISTRO'].count()) )
+  positive_death_df = death_df[death_df['RESULTADO'] == 1]
+  print("Defunciones positivas   ||  {}".format(positive_death_df['ID_REGISTRO'].count()) ) 
+  suspect_death_df = death_df[death_df['RESULTADO'] == 3]
+  print("Defunciones sospechosas || {}".format(suspect_death_df['ID_REGISTRO'].count()) )
 
+  death_series_full = death_df.groupby('FECHA_DEF')
+  death_series_positive = positive_death_df.groupby('FECHA_DEF')
+  death_series_suspect = suspect_death_df.groupby('FECHA_DEF')
+  
+  coalesced_deaths = pd.DataFrame(columns=["Positivos", "Sospechosos"])  
+  coalesced_deaths['Positivos'] = death_series_positive['ID_REGISTRO'].count()
+  coalesced_deaths['Sospechosos'] = death_series_suspect['ID_REGISTRO'].count()
+   
+  coalesced_df = pd.DataFrame(columns=["Positivos", "Sospechosos"])
   #coalesced_df['Total'] = timeseries_full['ID_REGISTRO'].count().cumsum()
   #coalesced_df['Negativos'] = timeseries_negative['ID_REGISTRO'].count().cumsum()
-  #coalesced_df['Pendientes'] = timeseries_pending['ID_REGISTRO'].count()
+  coalesced_df['Sospechosos'] = timeseries_pending['ID_REGISTRO'].count()
   coalesced_df['Positivos'] = timeseries_positive['ID_REGISTRO'].count()
-  coalesced_df['Defunciones'] = death_df['ID_REGISTRO'].count()
+  
+  #Fill days with no data or cases
+  coalesced_deaths.fillna(0,inplace=True)
   coalesced_df.fillna(0,inplace=True)
-  
-  cumulative_df = coalesced_df.cumsum()
 
+  #Plotting
   plt.style.use('ggplot')
-  if parsed.acumulados:
-     ax = cumulative_df.plot(grid=True)
-  else:
-     ax = coalesced_df.plot(grid=True)
-  ax.grid('on')
-  ax.set_title('Casos {}'.format(entidad))
+  graph_df(coalesced_df,cumulative=parsed.acumulados, title="Cases {}".format(entidad))
+  graph_df(coalesced_deaths,cumulative=parsed.acumulados, title="Defunciones {}".format(entidad))
   plt.show()
-  
