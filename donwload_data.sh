248bfad353b7cbe0dffa4f8ef50f817cf9f9198f
JesusCoyotzi <jesus_coyotzi@hotmail.com>
2020-09-12 17:57:20 -0500

    added date of ingress to options

--- donwload_data.sh
+++ donwload_data.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 rm -f *zip
-wget "http://187.191.75.115/gobmx/salud/datos_abiertos/datos_abiertos_covid19.zip"
+#List of previously working addresses for the record
+#wget "http://187.191.75.115/gobmx/salud/datos_abiertos/datos_abiertos_covid19.zip"
+#wget "http://epidemiologia.salud.gob.mx/gobmx/salud/datos_abiertos/datos_abiertos_covid19.zip"
+wget http://datosabiertos.salud.gob.mx/gobmx/salud/datos_abiertos/datos_abiertos_covid19.zip
 unzip "datos_abiertos_covid19.zip"
 mv *.csv csv_data
+
 
